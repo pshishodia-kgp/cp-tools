bf4ba1e6fb66d9147cde452b4ee877aa8fe0a263
Prashant Shishodia <prashantshishodia.dakshana17@gmail.com>
2020-03-17 16:37:56 +0530

    Added style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,10 @@ const NavBar = () => {
     return (
         <Navbar bg="light" variant="light">
             <Navbar.Brand href="/"><img src = {Logo} alt = "CP-Tools"/>   </Navbar.Brand>
-            <Nav className="justify-content-end" variant = "pills">
-                <Nav.Item> <Nav.Link href="/">Home</Nav.Link> </Nav.Item>
-                <Nav.Item> <Nav.Link href="/"> Problem Filter</Nav.Link> </Nav.Item>
-                <Nav.Item> <Nav.Link href="/">Recommendation</Nav.Link> </Nav.Item>
+            <Nav activekey = "/home" className="justify" variant = "pills">
+                <Nav.Item> <Nav.Link eventKey="home" href="/">Home</Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link eventKey="ProblemFilter" href="/"> Problem Filter</Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link eventKey="Recommendation" href="/">Recommendation</Nav.Link> </Nav.Item>
             </Nav>
       </Navbar>
     )
