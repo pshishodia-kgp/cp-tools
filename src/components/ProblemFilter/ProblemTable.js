18428112aa04bde5ea523f6f505bd70e9a84b193
Prashant Shishodia <prashantshishodia.dakshana17@gmail.com>
2020-04-16 17:57:30 +0530

    solved, unsolved + bug handling

--- src/components/ProblemFilter/ProblemTable.js
+++ src/components/ProblemFilter/ProblemTable.js
@@ -1,9 +1,8 @@
 import React from 'react'; 
-import {Table, Button} from 'react-bootstrap'; 
+import {Table, Button, ButtonGroup} from 'react-bootstrap'; 
 
 export default class ProblemTable extends React.Component{
     constructor(props){
-        //console.log('props : ', props); 
         super(props); 
         this.state = {
             tags : true,
@@ -11,30 +10,55 @@ export default class ProblemTable extends React.Component{
                 round : false,
                 name : true, 
                 rating : true,
+                solvedBy : true,
             },
+            mode : 'all',
+            problems : this.props.problems,
             sortActive : 'round', 
         }
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(this.props.problems !== nextProps.problems){
+            this.setState({ 
+                problems : nextProps.problems,
+            }, this.changeMode('all'));
+        }
+    }
+    triedCount = (problemName) => {
+        return (problemName in this.props.tried)? this.props.tried[problemName] : 0; 
+    }
+    solvedCount = (problemName) => {
+        return (problemName in this.props.solved)? this.props.solved[problemName] : 0; 
     }
 
     handleSorting = (fieldName) => {
         let problems, sortActive, sortOrder = this.state.sortOrder;  
         if(fieldName === 'round'){
-            problems = this.props.problems.sort(function(a, b){
+            problems = this.state.problems.sort((a, b) => {
                 if(a.contestId === b.contestId)return  (a.index < b.index ? -1 : 1); 
                 return parseInt(a.contestId) - parseInt(b.contestId); 
             });
 
         }else if(fieldName === 'name'){
-            problems = this.props.problems.sort(function(a, b){
+            problems = this.state.problems.sort((a, b) => {
                 if(a.name < b.name)return -1; 
                 else if(a.name >  b.name)return 1; 
                 else return 0; 
             });
         }else if(fieldName === 'rating'){
-            problems = this.props.problems.sort(function(a, b){
+            problems = this.state.problems.sort((a, b) => {
                 if(a.rating === b.rating)return  parseInt(b.contestId) - parseInt(a.contestId); 
                 return parseInt(a.rating) - parseInt(b.rating); 
             });
+        }else if(fieldName === 'solvedBy'){
+            problems = this.state.problems.sort((a, b) => {
+                if(this.triedCount(a.name) === 0 && this.triedCount(b.name) === 0)return 0; 
+                if(this.triedCount(a.name) === 0)return 1; 
+                if(this.triedCount(b.name) === 0)return -1; 
+                if(this.solvedCount(a.name) === this.solvedCount(b.name))return this.triedCount(a.name) - this.triedCount(b.name); 
+                return this.solvedCount(a.name) - this.solvedCount(b.name); 
+            })
         }
 
         if(!sortOrder[fieldName]){
@@ -72,19 +96,29 @@ export default class ProblemTable extends React.Component{
                     <th>
                         Rating
                         {this.getIcon('rating')}
-                    </th> 
+                    </th>
+
+                    <th> 
+                        Solved/Tried
+                        {this.getIcon('solvedBy')}
+                    </th>  
                 </tr> 
             </thead> 
         )
     }
 
     tableRows = () => {
-        return this.props.problems.map((problem) => {
+        return this.state.problems.map((problem) => {
             let url = "https://codeforces.com/problemset/problem/" + problem.contestId.toString() + '/' + problem.index.toString(); 
             let tagList = (!this.state.tags)? (<div> </div>)
-                                            : problem.tags.map((tag) => <span style = {{'text-decorations' : 'none'}}>{tag}, </span> );
+                                            : problem.tags.map((tag) => <span className = "text-muted" style = {{'text-decorations' : 'none'}}>{tag}, </span> );
+            
+            let solvedCol = (this.triedCount(problem.name))? this.solvedCount(problem.name) + '/' + this.triedCount(problem.name) : '';
+            let status = ''; 
+            if(this.triedCount(problem.name) > 0 && this.solvedCount(problem.name) === 0)status = 'rejected-problem'; 
+            if(this.triedCount(problem.name) > 0 && this.solvedCount(problem.name) === this.triedCount(problem.name))status = 'accepted-problem';
             return(
-                <tr> 
+                <tr > 
                     <td> {problem.contestId.toString() + problem.index.toString()} </td> 
                     <td>
                         <div style={{'float' : 'left'}}>
@@ -94,25 +128,59 @@ export default class ProblemTable extends React.Component{
                             {tagList}
                         </div>
                     </td>
-                    <td> {problem.rating} </td> 
+                    <td> {problem.rating} </td>
+                    <td className = {status}> {solvedCol} </td> 
                 </tr> 
             )
         });
     }
-    
+
+    changeMode = (mode) => {
+        let problems = []; 
+        if(mode === 'all'){
+            problems = this.props.problems; 
+        }else if(mode === 'solved'){
+            problems = this.state.problems.filter((problem) => {
+                return this.triedCount(problem.name) > 0 && this.triedCount(problem.name) === this.solvedCount(problem.name); 
+            });
+        }else if(mode === 'unsolved'){
+            problems = this.state.problems.filter((problem) => {
+                return this.solvedCount(problem.name) === 0; 
+            });
+        }
+
+        this.setState({
+            problems : problems, 
+            mode : mode,
+        })
+    } 
+
     render(){
-        if(!this.props.problems){
+        if(!this.state.problems){
             return (<div> </div> ); 
         }
 
+        let modeVariant = { 'solved' : 'outline-info', 
+                            'unsolved' : 'outline-info',
+                            'all' : 'outline-info',}
+
+        modeVariant[this.state.mode] = 'info'; 
+
         return (
-            <div> 
-                <span>Total problems : {this.props.problems.length} </span> 
-            <Table className = "table" striped bordered style = {{borderRadius : '5px'}}> 
-                {this.tableHeader()}
-                <tbody> {this.tableRows()} </tbody>
-            </Table> 
-            </div> 
+            <React.Fragment> 
+                {/* <span>Total problems : {this.state.problems.length} </span>  */}
+
+                <ButtonGroup> 
+                    <Button onClick = {() => this.changeMode('solved')} variant = {modeVariant['solved']}> Solved</Button>
+                    <Button onClick = {() => this.changeMode('unsolved')} variant = {modeVariant['unsolved']}> Unsolved </Button> 
+                    <Button onClick = {() => this.changeMode('all')} variant = {modeVariant['all']}> All </Button>
+                </ButtonGroup> 
+                
+                <Table className = "table" striped bordered style = {{borderRadius : '5px'}}> 
+                    {this.tableHeader()}
+                    <tbody> {this.tableRows()} </tbody>
+                </Table> 
+            </React.Fragment> 
         )
     }
 }
